db5e710d46b253081863f690565ef2e5f380a4c4
Rob Bieman <a@a.com>
2025-07-12 15:25:10 +0200

    experiment for vit on imagenet

--- hardware.py
+++ hardware.py
@@ -11,4 +11,16 @@ class HardwareConfig(utils.SelfDescripting):
     node_count = 1
 
     def format_as_slurm_args(self) -> str:
-        return f"-N {self.node_count} -p {self.partition} -t {self.time} --gpus-per-node={self.gpu_count}"
+        return f"-N {self.node_count} -p {self.partition} -t {self.time} --gpus-per-node={self.gpu_count} --mem=0"
+
+
+class CurrentDevice:
+    hardware: HardwareConfig
+
+    @staticmethod
+    def set_hardware(hw: HardwareConfig):
+        CurrentDevice.hardware = hw
+
+    @staticmethod
+    def get_hardware() -> HardwareConfig:
+        return CurrentDevice.hardware
